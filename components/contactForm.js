39cd354f17aeffa6c4a9509abaafd0ef240d1a81
bhaskarandmr <99174239+bhaskarandmr@users.noreply.github.com>
2022-11-14 17:46:50 +0530

    Update contactForm.js

--- components/contactForm.js
+++ components/contactForm.js
@@ -176,12 +176,12 @@ const ContactForm = () => {
     <>
     <Svg/>
     <div className="p-3 pb-md-4 mt-3 mx-auto text-center container">
-        <svg xmlns="http://www.w3.org/2000/svg" width="46" height="46" fill="currentColor" className="bi bi-envelope-paper" viewBox="0 0 16 16">
-            <path d="M4 0a2 2 0 0 0-2 2v1.133l-.941.502A2 2 0 0 0 0 5.4V14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V5.4a2 2 0 0 0-1.059-1.765L14 3.133V2a2 2 0 0 0-2-2H4Zm10 4.267.47.25A1 1 0 0 1 15 5.4v.817l-1 .6v-2.55Zm-1 3.15-3.75 2.25L8 8.917l-1.25.75L3 7.417V2a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v5.417Zm-11-.6-1-.6V5.4a1 1 0 0 1 .53-.882L2 4.267v2.55Zm13 .566v5.734l-4.778-2.867L15 7.383Zm-.035 6.88A1 1 0 0 1 14 15H2a1 1 0 0 1-.965-.738L8 10.083l6.965 4.18ZM1 13.116V7.383l4.778 2.867L1 13.117Z"/>
-        </svg>
-        <h1 className="display-4 fw-normal">Get In Touch</h1>
-        <p className="fs-5 text-muted">Leave me a message in the contact form below.</p>
-        <p>I will answer you as soon as possible.</p>
+      
+        <h1 className="display-4 fw-normal">
+இது உங்களுக்கான வழிகாட்டி நிகழ்ச்சி
+</h1>
+        <p className="fs-5 text-muted">Register for FREE</p>
+        <p>Get your guidance on IAS studies</p>
     </div>
 
     <div className="col-lg-6 col-md-6 mt-1 container">
